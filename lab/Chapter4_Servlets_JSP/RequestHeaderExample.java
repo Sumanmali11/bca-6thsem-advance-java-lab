d089656184eb31c74a5612f753188620f2974025
Mali Suman <sumanmali1167@gmail.com>
2025-04-08 08:19:52 +0545

    Servlets

---
+++ lab/Chapter4_Servlets_JSP/RequestHeaderExample.java
@@ -0,0 +1,21 @@
+
+import java.io.*;
+import java.util.*;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+
+public class RequestHeaderExample extends HttpServlet {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws IOException, ServletException
+    {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        Enumeration e = request.getHeaderNames();
+        while (e.hasMoreElements()) {
+            String name = (String)e.nextElement();
+            String value = request.getHeader(name);
+            out.println(name + " = " + value);
+        }
+    }
+}
--- lab/Chapter4_Servlets_JSP/index.jsp
+++
@@ -1,7 +0,0 @@
-<html>
-<head><title>JSP Example</title></head>
-<body>
-    <h1>Welcome to JSP!</h1>
-    <p>Current Time: <%= new java.util.Date() %></p>
-</body>
-</html>
